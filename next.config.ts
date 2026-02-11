a15758fc4ae3f59cd897f95c2842bd805c26aaf5
Nasre ff <nasredebbah617@gmail.com>
2026-02-11 03:33:03 +0000

    "Add a Content Security Policy (CSP) to my next.config.js to prevent Cro

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type {NextConfig} from 'next';
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+    img-src 'self' blob: data: https://placehold.co https://images.unsplash.com https://picsum.photos https://image.tmdb.org https://api.qrserver.com;
+    font-src 'self' https://fonts.gstatic.com;
+    connect-src 'self' https://api.themoviedb.org wss://*.supabase.co https://*.supabase.co https://vitals.vercel-insights.com;
+    frame-src 'self' https://vidsrc.icu;
+`.replace(/\s{2,}/g, ' ').trim();
+
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader,
+          },
+        ],
+      },
+    ];
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
