a241edf1aff9e75d50161720b68b41cd6c857f81
Nasre ff <nasredebbah617@gmail.com>
2026-02-11 02:16:56 +0000

    "Update the global CSS to support multi-language typography. Import 'Cai

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
+        body: ['Inter', 'Cairo', 'sans-serif'],
+        headline: ['Inter', 'Cairo', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
